895602ec88be3872f52cbad4c654c186a0ef9388
Volodymyr Korniichuk <VolodymyrKorn@gmail.com>
2013-04-29 20:22:16 +0300

    fixed small bugs

--- MyRequestHandler.cpp
+++ MyRequestHandler.cpp
@@ -3,39 +3,46 @@
 #include <string>
 
 #include "MyRequestHandler.h"
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPRequestHandler.h>
 #include <Poco/Net/HTTPServerResponse.h>
-#include <Poco/Net/HTMLForm.h>
 #include <Poco/Net/HTTPServer.h>
 #include <Poco/Net/HTTPRequestHandlerFactory.h>
-#include <Poco/Mutex.h>
 #include <Poco/ScopedLock.h>
 #include <Poco/URI.h>
 #include <Poco/StringTokenizer.h>
 
+using namespace Poco::Net;
+
 string MyServerApp::text = "Hello world!";
+Mutex MyServerApp::textLock;
 
-void CMyRequestHandler::handleRequest(HTTPServerRequest &req, HTTPServerResponse &resp)
+class CMyRequestHandler : public HTTPRequestHandler
 {
-	resp.setStatus(HTTPResponse::HTTP_OK);
-	resp.setContentType("text/html");
-	ostream& out = resp.send();
-
-	URI uri(req.getURI());
-	if (uri.toString().find("/setText") == 0)
+public:
+	void handleRequest(HTTPServerRequest &req, HTTPServerResponse &resp)
 	{
-		StringTokenizer str(uri.getQuery(), "=");
-		if (str.count() == 2 && str[0] == "text")
-		{
-			MyServerApp::setText(str[1]);
-			out << "ok";
-			out.flush();
-			return;
-		}
-	}
+		resp.setStatus(HTTPResponse::HTTP_OK);
+		resp.setContentType("text/html");
+		ostream& out = resp.send();
 
-	out << "error";
-	out.flush();
-}
+		URI uri(req.getURI());
+		if (uri.toString().find("/setText") == 0)
+		{
+			StringTokenizer str(uri.getQuery(), "=");
+			if (str.count() == 2 && str[0] == "text")
+			{
+				MyServerApp::setText(str[1]);
+				out << "ok";
+				out.flush();
+				return;
+			}
+		}
+
+		out << "error";
+		out.flush();
+	}
+};
 
 class MyRequestHandlerFactory : public HTTPRequestHandlerFactory
 {
@@ -48,9 +55,14 @@ public:
 
 void MyServerApp::setText(string newText)
 {
-	Mutex textLock;
 	ScopedLock<Mutex> lock(textLock);
 	text = newText;
+}
+
+string MyServerApp::getText() 
+{ 
+	ScopedLock<Mutex> lock(textLock);
+	return text; 
 }
 
 int MyServerApp::main(const vector<string> &)
--- MyRequestHandler.h
+++ MyRequestHandler.h
@@ -1,27 +1,20 @@
 #pragma once
 
-#include <Poco/Net/HTTPServerRequest.h>
-#include <Poco/Net/HTTPRequestHandler.h>
+#include <Poco/Mutex.h>
 #include <Poco/Util/ServerApplication.h>
 
-using namespace Poco::Net;
-using namespace Poco::Util;
 using namespace Poco;
+using namespace Poco::Util;
 using namespace std;
-
-class CMyRequestHandler : public HTTPRequestHandler
-{
-public:
-	void handleRequest(HTTPServerRequest &req, HTTPServerResponse &resp);
-};
 
 class MyServerApp : public ServerApplication
 {
 public:
+	static string getText();
 	static void setText(string newText);
-	static string getText() { return text; }
 
 protected:
 	int main(const vector<string> &);
 	static string text;
+	static Mutex textLock;
 };
